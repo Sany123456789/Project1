889f3cebcd1b90bc222ac5362244a79edab8776b
sangamesh <sangameshsc44@gmail.com>
2024-11-22 12:30:59 +0530

    successfully resolved

--- Demo.java
+++ Demo.java
@@ -1,10 +1,9 @@
 class Demo{
     public static void main(String[]args){
         System.out.println("HI WELCOME TO GITHUB")
-<<<<<<< HEAD
         Sytem.out.println("hope you will doing good")
-=======
+
        
->>>>>>> ba68fc787b08d476f52dd5dc06b692a2af088c4d
+
     }
 }
